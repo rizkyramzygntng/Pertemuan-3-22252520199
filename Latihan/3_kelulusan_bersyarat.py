abde9916352437f4b451c7b85d1a83e6ecb2aca2
FikriYustiawann <fikriyustiawan13@gmail.com>
2026-09-17 17:55:00 +0700

    feat: menambahkan latihan seleksi pertemuan 03

---
+++ Latihan/3_kelulusan_bersyarat.py
@@ -0,0 +1,9 @@
+# Program menentukan kelulusan berdasarkan nilai dan kehadiran
+
+nilai = float(input("Nilai akhir: "))
+kehadiran = float(input("Kehadiran (%): "))
+
+if nilai >= 60 and kehadiran >= 80:
+    print("Lulus")
+else:
+    print("Belum lulus")
